c13f3b6cde4d0c8e5e86ddf0c34033b67205bc0f
Cihan Bebek <cihan.bebek@uta.fi>
2014-03-10 22:08:23 +0200

    added mongoose-auto-incrementation, comments
    Added usage of mongoose-auto-incrementation for Schemas.
    Also added commenting here and there.

--- Eventshuffle.js
+++ Eventshuffle.js
@@ -1,9 +1,19 @@
+/*
+* Dunno if I should comment in finnish or english, I chose the latter.
+* 
+*/
+
 var express = require('express'),
     path = require('path'),
     http = require('http'),
-    mongoose = require('mongoose');
+    mongoose = require('mongoose'),
+    autoIncrement = require('mongoose-auto-increment');
 
 var app = express();
+
+/*We need these to use mongoose-auto-increment*/
+var connection = mongoose.createConnection("mongodb://localhost/myDatabase");
+autoIncrement.initialize(connection);
 
 app.configure(function () {
     app.set('port', process.env.PORT || 3000);
@@ -13,25 +23,45 @@ app.configure(function () {
     mongoose.connect('mongodb://localhost/eventsdb');
 });
 
+/*Schema for events*/
 var eventSchema = new mongoose.Schema({
+    //name of the event
     name: {
         type: String,
         required: true
     },
     dates: {
+        //Gotta find a better object for date, so we can get rid of time at end 
+        //we could use String but thats not cool
         type: [Date],
         default: []
     }
 });
-
 var Event = mongoose.model('Event', eventSchema);
+eventSchema.plugin(autoIncrement.plugin, 'Event');
+
+/*Schema for votes*/
+var voteSchema = new mongoose.Schema({
+    //Name of the person posting the vote
+    name: {
+        type: String,
+        required: true
+    },
+    votes: {
+        type: [Date],
+        default: []
+    }
+});
+var Vote = mongoose.model('Vote', voteSchema);
+voteSchema.plugin(autoIncrement.plugin, 'Vote');
+
 
 /*Posts new event to db*/
 app.post('/events/', function(req, res) {
-    // Create new instance of Event-model, with the sent data (req.body)
+    //Create new instance of Event-model, with the sent data (req.body)
     var event = new Event(req.body);
 
-    // Tallennetaan
+    //Save it
     event.save(function(err, event) {
         res.send(201, event);
     });

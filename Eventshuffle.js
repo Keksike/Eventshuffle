dec75e1db22b1398a928273c002f283dd6f3b930
Keksike <cihan.bebek@uta.fi>
2014-03-10 20:39:17 +0200

    Added missing POST & GET -commands

--- Eventshuffle.js
+++ Eventshuffle.js
@@ -14,46 +14,64 @@ app.configure(function () {
 });
 
 var eventSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  dates: {
-    type: [Date],
-    default: []
-  }
+    name: {
+        type: String,
+        required: true
+    },
+    dates: {
+        type: [Date],
+        default: []
+    }
 });
 
 var Event = mongoose.model('Event', eventSchema);
 
-app.get('/events/list', function(req, res) {
-  Event.find({}, function(err, events) {
-    res.send(events);
-  });
-});
-
-app.get('/events/:id', function(req, res) {
-  Event.findOne({ _id: req.params.id }, function(err, event) {
-    if(event == null) {
-      return res.send(404, 'Event not found!');
-    }
-    res.send(event);
-  });
-});
-
+/*Posts new event to db*/
 app.post('/events/', function(req, res) {
-  // Luodaan uusi Event modelin instanssi lähetetystä datasta (req.body)
-  var event = new Event(req.body);
+    // Create new instance of Event-model, with the sent data (req.body)
+    var event = new Event(req.body);
 
-  // Tallennetaan
-  event.save(function(err, event) {
-    res.send(201, event);
-  });
+    // Tallennetaan
+    event.save(function(err, event) {
+        res.send(201, event);
+    });
 });
 
-// app.post('/events/:id/vote', events.vote);
-// app.get('events/:id/results', events.getResults);
+/*Posts new vote into event with its ID*/
+app.post('/events/:id/vote', function(req, res) {
+    var event = new Event(req.body);
 
+    event.save(function(err, event) {
+        res.send(201, event);
+    });
+});
+
+/*Lists all events in db*/
+app.get('/events/list', function(req, res) {
+    Event.find({}, function(err, events) {
+        res.send(events);
+    });
+});
+
+/*Gets a single event with its ID*/
+app.get('/events/:id', function(req, res) {
+    Event.findOne({ _id: req.params.id }, function(err, event) {
+        if(event == null) {
+             return res.send(404, 'Event not found!');
+        }
+        res.send(event);
+    });
+});
+
+/*Gets the results of an event with its ID*/
+app.get('/events/:id', function(req, res) {
+    Event.findOne({ _id: req.params.id }, function(err, event) {
+        if(event == null) {
+            return res.send(404, 'Event not found!');
+        }
+        res.send(event);
+    });
+});
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log("Listening on port " + app.get('port'));
